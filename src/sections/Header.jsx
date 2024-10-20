736ad2c76072338e72a176e2fa3391605ba9f44b
Luis Alejandro Monroy <alejo_un@hotmail.com>
2024-10-20 01:16:33 -0700

    commit 19-10-2024

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,8 @@
+import './Header.css'
 import avatar from '../assets/images/avatar.jpg'
+import linkedin_ico from '../assets/icons/linkedin.svg'
+import github_ico from '../assets/icons/github.svg'
+import email_ico from '../assets/icons/email.svg'
 
 const SOCIAL_LINKS = {
     linkedin: "https://www.linkedin.com/in/alejandro-monroy-dev/",
@@ -12,20 +16,24 @@ export function Header(){
             <header>
                 <span className="image avatar"><img src={avatar} alt="" /></span>
                 <h1 id="logo"><a href="#">Ing. Luis Alejandro Monroy</a></h1>
-                <p>Hardware & Firmware developper<br/>with open work permit</p>
             </header>
 
             <nav id="nav">
                 <ul>
                     <li><a href="#one" className="active">About</a></li>
-                    <li><a href="#two">Things I Can Do</a></li>
-                    <li><a href="#three">A Few Accomplishments</a></li>
+                    <li><a href="#two">Skills</a></li>
+                    <li><a href="#three">Experience</a></li>
+                    <li><a href="#four">Projets</a></li>
+                    <li><a href="#four">Education</a></li>
                     <li><a href="#four">Contact</a></li>
                 </ul>
             </nav>
 
             <footer>
                 <ul className="icons">
+                    <li><a href={SOCIAL_LINKS.linkedin}><img src={linkedin_ico} alt=""/><span className="label">Instagram</span></a></li>
+                    <li><a href={SOCIAL_LINKS.github}><img src={github_ico} alt=""/><span className="label">Github</span></a></li>
+                    <li><a href={SOCIAL_LINKS.email}><img src={email_ico} alt=""/><span className="label">Email</span></a></li>
                 </ul>
             </footer>
         </section>
